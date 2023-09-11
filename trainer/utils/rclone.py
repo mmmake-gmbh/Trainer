84999c2c588875d396ff2148c7f1251b64bbbc4e
Andreas Klos <andreas.klos@camao.one>
2023-09-11 16:05:37 +0200

    ADJUST stuff

--- trainer/utils/rclone.py
+++ trainer/utils/rclone.py
@@ -8,8 +8,8 @@ def get_data_from_lakefs(repo_name:str, branch_name:str, target_folder:str):
     print("Rclone stderr:", result.stderr)
     assert result.returncode == 0
     
-def sync_data2s3bucket(bucket_name:str, source_folder:str):
-    command = ["rclone", "sync", source_folder, f"s3:{bucket_name}"]
+def sync_data2s3bucket(bucket_name:str, source_folder:str, name_from_config:str="lakefs"):
+    command = ["rclone", "sync", source_folder, f"{name_from_config}:{bucket_name}"]
     result = subprocess.run(command, capture_output=True)
     print("Rclone stderr:", result.stderr)
     assert result.returncode == 0
