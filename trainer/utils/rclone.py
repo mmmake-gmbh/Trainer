f1725a865ae14e23ac745143ad60574628751e63
Andreas Klos <andreas.klos@camao.one>
2023-09-14 15:36:03 +0200

    ADJUST copy method

--- trainer/utils/rclone.py
+++ trainer/utils/rclone.py
@@ -15,7 +15,12 @@ def sync_data2s3bucket(bucket_name:str, source_folder:str, name_from_config:str=
     assert result.returncode == 0
     
 def copy_data_from_s3bucket(bucket_uri:str, target_folder:str):
+    splitted_bucket_uri = [s for s in bucket_uri.split("/") if s != ""]
+    if len(splitted_bucket_uri) > 1:
+        #assumption: only one folder is specified in the bucket_uri
+        target_folder = os.path.join(target_folder,splitted_bucket_uri[-1])
     command = ["rclone", "copy", bucket_uri, target_folder]
     result = subprocess.run(command, capture_output=True)
     print("Rclone stderr:", result.stderr)
     assert result.returncode == 0
+    return target_folder
